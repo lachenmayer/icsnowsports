2a3565b956ac81d6bf63c29b26e162aefeb4813c
Harry Lachenmayer <harrylachenmayer@gmail.com>
2013-10-17 20:01:09 +0100

    Quick clean up

--- watch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SRC_DIR=.
-
-sass --watch $SRC_DIR/css:$SRC_DIR/bin/css &
-sass_pid=${!}
-coffee -w -o $SRC_DIR/bin/js -c $SRC_DIR/js &
-coffee_pid=${!}
-wait $sass_pid $coffee_pid
